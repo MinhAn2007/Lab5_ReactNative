a80260428d61dd8378b6fda1d4b181b4766f8373
minhan2007 <voongocminhan20072002@gmail.com>
2023-10-09 14:15:26 +0700

    init

---
+++ src/components/OrderScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const OrderScreen = () => {
+    return (
+        <View style= {styles.container}>
+            <Text>KASkbaksdbkajsd</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  export default OrderScreen
